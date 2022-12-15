d192eee98029151c6678a553221abf037f0631aa
Dev Shopify <dev.xovakstudio@gmail.com>
2022-12-16 02:25:42 +0530

    menu grid
    menu grid

---
+++ assets/menu-grid.js
@@ -0,0 +1,20 @@
+new Swiper('.menu-grid_slider', {
+    // Default parameters
+    slidesPerView: 1,
+    direction: 'vertical',
+    spaceBetween: 0,
+    loop: !0,
+    mousewheel: true,
+    keyboard: {
+        enabled: true,
+    },
+    
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      }
+    }
+  })
