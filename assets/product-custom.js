f56645e92a0c92f2214191d9f69667a78371e16c
VishalAumento <dev.xovakstudio@gmail.com>
2022-12-19 19:18:49 +0530

    PDP page
    PDP page

---
+++ assets/product-custom.js
@@ -0,0 +1,32 @@
+
+new Swiper(".product_page_slider_image", {
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween: 35,
+    autoHeight: false,
+    //grabCursor: true,
+    //centeredSlides: true,
+    autoplay: {
+      delay: 1000,
+    },
+    breakpoints: {
+    991: {
+      autoplay: {
+      delay: 10000,
+    },
+    },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: 'bullets',
+    },
+    renderBullet: function (index, className) {
+      return `<span class="outer-dot swiper-pagination-bullet"><span class="inner-dot"></span></span>`;
+    },
+    navigation: {
+      nextEl: ".swiper--next",
+      prevEl: ".swiper--prev",
+    },
+    
+  });
