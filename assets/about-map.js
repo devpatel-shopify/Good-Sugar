632f347c2ba7ce608265af58973043c4bcd9be81
VishalAumento <dev.xovakstudio@gmail.com>
2022-12-27 16:29:19 +0530

    About page
    About page

--- assets/about-map.js
+++ assets/about-map.js
@@ -17,24 +17,24 @@ window.onscroll = function() {
 
 new Swiper('.image_text_slide_main', {
     // Default parameters
-    slidesPerView: 'auto',
+    slidesPerView:1,
     spaceBetween: 0,
     mousewheel: true,
-    autoHeight: true,
     mousewheel: {
       forceToAxis: false,
       releaseOnEdges: true,
     },
-    centeredSlides: true,
+    //noSwiping: false,
     keyboard: {
         enabled: true,
-    },    
+    },   
     // Responsive breakpoints
     breakpoints: {
       // when window width is >= 640px
       750: {
         slidesPerView: 1.8,
-        spaceBetween:0,
+        spaceBetween: 0,
+        autoHeight:100,
       }
     }
   })
