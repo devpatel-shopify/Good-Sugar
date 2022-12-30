36567c197c9b7e882ed269bc9904ab962f3a08b3
VishalAumento <dev.xovakstudio@gmail.com>
2022-12-30 13:05:59 +0530

    Resources
    Resources

--- assets/image-horizontal-slider.js
+++ assets/image-horizontal-slider.js
@@ -9,7 +9,7 @@ new Swiper('.image_horizontal_slider_main', {
       releaseOnEdges: true,
     },
     autoHeight:100,
-    centeredSlides: true,
+    //centeredSlides: true,
     keyboard: {
         enabled: true,
     },    
@@ -23,4 +23,58 @@ new Swiper('.image_horizontal_slider_main', {
       }
     }
   })
+}
+
+if('.image_horizontal_slider_main_tablet'){
+  new Swiper('.image_horizontal_slider_main_tablet', {
+      // Default parameters
+      slidesPerView: 1.8,
+      spaceBetween: 30,
+      mousewheel: true,
+      mousewheel: {
+        forceToAxis: false,
+        releaseOnEdges: true,
+      },
+      autoHeight:100,
+      //centeredSlides: true,
+      keyboard: {
+          enabled: true,
+      },
+      // Responsive breakpoints
+      breakpoints: {
+        // when window width is >= 640px
+        750: {
+          slidesPerView: 3,
+          spaceBetween: 60,
+          autoHeight:100,
+        }
+      }
+    })
+  }
+
+if('.image_horizontal_slider_main_mobile'){
+  new Swiper('.image_horizontal_slider_main_mobile', {
+   // Default parameters
+   slidesPerView: 1.8,
+   spaceBetween: 30,
+   mousewheel: true,
+   mousewheel: {
+     forceToAxis: false,
+     releaseOnEdges: true,
+   },
+   autoHeight:100,
+   //centeredSlides: true,
+   keyboard: {
+       enabled: true,
+   },
+   // Responsive breakpoints
+   breakpoints: {
+     // when window width is >= 640px
+     750: {
+       slidesPerView: 4,
+       spaceBetween: 60,
+       autoHeight:100,
+     }
+   }
+ })
 }
