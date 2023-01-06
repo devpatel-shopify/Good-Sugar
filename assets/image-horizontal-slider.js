5742011f651e53aa7a201f4c0246b84ead287dc9
VishalAumento <dev.xovakstudio@gmail.com>
2023-01-06 16:04:16 +0530

    about + resources
    bug solve and image slider change on flex.

--- assets/image-horizontal-slider.js
+++ assets/image-horizontal-slider.js
@@ -1,90 +1,90 @@
-if('.image_horizontal_slider_main'){
-new Swiper('.image_horizontal_slider_main', {
-    // Default parameters
-    slidesPerView: 1.8,
-    spaceBetween: 30,
-    mousewheel: true,
-    mousewheel: {
-      forceToAxis: false,
-      releaseOnEdges: true,
-    },
-    autoHeight:100,
-    //centeredSlides: true,
-    keyboard: {
-        enabled: true,
-    },    
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 640px
-      750: {
-        slidesPerView: 3,
-        spaceBetween: 60,
-        autoHeight:100,
-      },
-      990: {
-        slidesPerView: 3.5,
-        spaceBetween: 60,
-        autoHeight:100,
-      },
-      1550: {
-        slidesPerView: 3.5,
-        spaceBetween: 60,
-        autoHeight:100,
-      }
-    }
-  })
-}
+// if('.image_horizontal_slider_main'){
+// new Swiper('.image_horizontal_slider_main', {
+//     // Default parameters
+//     slidesPerView: 1.8,
+//     spaceBetween: 30,
+//     mousewheel: true,
+//     mousewheel: {
+//       forceToAxis: false,
+//       releaseOnEdges: true,
+//     },
+//     autoHeight:100,
+//     //centeredSlides: true,
+//     keyboard: {
+//         enabled: true,
+//     },    
+//     // Responsive breakpoints
+//     breakpoints: {
+//       // when window width is >= 640px
+//       750: {
+//         slidesPerView: 3,
+//         spaceBetween: 60,
+//         autoHeight:100,
+//       },
+//       990: {
+//         slidesPerView: 3.5,
+//         spaceBetween: 60,
+//         autoHeight:100,
+//       },
+//       1550: {
+//         slidesPerView: 3.5,
+//         spaceBetween: 60,
+//         autoHeight:100,
+//       }
+//     }
+//   })
+// }
 
-if('.image_horizontal_slider_main_tablet'){
-  new Swiper('.image_horizontal_slider_main_tablet', {
-      // Default parameters
-      slidesPerView: 1.8,
-      spaceBetween: 30,
-      mousewheel: true,
-      mousewheel: {
-        forceToAxis: false,
-        releaseOnEdges: true,
-      },
-      autoHeight:100,
-      //centeredSlides: true,
-      keyboard: {
-          enabled: true,
-      },
-      // Responsive breakpoints
-      breakpoints: {
-        // when window width is >= 640px
-        750: {
-          slidesPerView: 2.5,
-          spaceBetween: 60,
-          autoHeight:100,
-        }
-      }
-    })
-  }
+// if('.image_horizontal_slider_main_tablet'){
+//   new Swiper('.image_horizontal_slider_main_tablet', {
+//       // Default parameters
+//       slidesPerView: 1.8,
+//       spaceBetween: 30,
+//       mousewheel: true,
+//       mousewheel: {
+//         forceToAxis: false,
+//         releaseOnEdges: true,
+//       },
+//       autoHeight:100,
+//       //centeredSlides: true,
+//       keyboard: {
+//           enabled: true,
+//       },
+//       // Responsive breakpoints
+//       breakpoints: {
+//         // when window width is >= 640px
+//         750: {
+//           slidesPerView: 2.5,
+//           spaceBetween: 60,
+//           autoHeight:100,
+//         }
+//       }
+//     })
+//   }
 
-if('.image_horizontal_slider_main_mobile'){
-  new Swiper('.image_horizontal_slider_main_mobile', {
-   // Default parameters
-   slidesPerView: 1.8,
-   spaceBetween: 30,
-   mousewheel: true,
-   mousewheel: {
-     forceToAxis: false,
-     releaseOnEdges: true,
-   },
-   autoHeight:100,
-   //centeredSlides: true,
-   keyboard: {
-       enabled: true,
-   },
-   // Responsive breakpoints
-   breakpoints: {
-     // when window width is >= 640px
-     750: {
-       slidesPerView: 4,
-       spaceBetween: 60,
-       autoHeight:100,
-     }
-   }
- })
-}
+// if('.image_horizontal_slider_main_mobile'){
+//   new Swiper('.image_horizontal_slider_main_mobile', {
+//    // Default parameters
+//    slidesPerView: 1.8,
+//    spaceBetween: 30,
+//    mousewheel: true,
+//    mousewheel: {
+//      forceToAxis: false,
+//      releaseOnEdges: true,
+//    },
+//    autoHeight:100,
+//    //centeredSlides: true,
+//    keyboard: {
+//        enabled: true,
+//    },
+//    // Responsive breakpoints
+//    breakpoints: {
+//      // when window width is >= 640px
+//      750: {
+//        slidesPerView: 4,
+//        spaceBetween: 60,
+//        autoHeight:100,
+//      }
+//    }
+//  })
+// }
