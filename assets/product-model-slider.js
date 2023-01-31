1f02faf3589413d384a8b9270802e1ba651c88a6
VishalAumento <dev.xovakstudio@gmail.com>
2023-01-31 19:25:01 +0530

    Product zoom slider
    - Product zoom slider add change into design

---
+++ assets/product-model-slider.js
@@ -0,0 +1,33 @@
+
+new Swiper(".product_model_slider", {
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween:0,
+    autoHeight: false,
+    //grabCursor: true,
+    //centeredSlides: true,
+    // autoplay: {
+    //   delay: 2000,
+    // },
+    breakpoints: {
+    750: {
+      slidesPerView: 1,
+    //   autoplay: {
+    //   delay: 10000,
+    // },
+    },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: 'bullets',
+    },
+    renderBullet: function (index, className) {
+      return `<span class="outer-dot swiper-pagination-bullet"><span class="inner-dot"></span></span>`;
+    },
+    navigation: {
+      nextEl: ".swiper--next",
+      prevEl: ".swiper--prev",
+    },
+    
+  });
