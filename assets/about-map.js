cbe4ee3073ef0c6ea0dfa521f4805c7cf60f4b9d
Aumento <dev.xovakstudio@gmail.com>
2023-02-17 16:23:29 +0530

    about slider
    - map slider design use js
    - image center slider design

--- assets/about-map.js
+++ assets/about-map.js
@@ -39,3 +39,77 @@ new Swiper('.image_text_slide_main', {
       }
     }
   })
+
+/*********************************************/
+
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+var headerHeight = document.querySelector("sticky-header").offsetHeight;
+/* Panels */
+let panels = gsap.utils.toArray("#slider-container .slider-item");
+let panelsContainer = document.querySelector("#slider-container");
+
+var containerWidth = 0 ;
+var slides = document.getElementsByClassName("slider-item");
+for (var i = 0; i < slides.length; i++) {
+  containerWidth += parseInt(slides.item(i).offsetWidth, 10);
+}
+console.log({
+  containerWidth:containerWidth,
+  headerHeight:headerHeight
+});
+panelsContainer.style.display  = "flex";
+panelsContainer.style.width  = containerWidth+"px";
+
+  gsap.to("#slider-container", {
+    xPercent: -100, 
+    x: () => innerWidth,
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#slider-container",
+      pin: "#MainContent",
+      start: "top center",
+      end: "center 100px",
+      //end: () => (innerWidth*2),
+      scrub: 3,
+      invalidateOnRefresh:true,
+      markers:true
+    }
+  });
+
+
+  gsap.to("#about_map_main", {
+    xPercent: -100, 
+    x: () => innerWidth,
+    //x: () => document.getElementsByClassName("about_map_wrapper").offsetWidth,
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#about_map_main",
+      pin: "#MainContent",
+      start: "top 148px",
+      end: "bottom center",
+      //end: () => (innerWidth*2),
+      scrub: 3,
+      invalidateOnRefresh:true,
+      markers:true
+    }
+  });
+
+
+  
+
+
+// let mm = gsap.matchMedia();
+
+// mm.add("(min-width: 1024px)", () => {
+//   // desktop setup code here...
+  
+// });
+
+// mm.add("(max-width: 1023px)", () => {
+//   // mobile setup code here...
+//   return function() {
+//       //gsap.kill(); 
+//       // other cleanup code can go here. 
+//     };
+// });
